8b8ca60fc013e7287d903350df0235845e07cb0f
JuanVPC <88857897+JuanVPC@users.noreply.github.com>
2022-11-16 16:01:29 -0300

    Update Main.java

--- Codigo Base TAD Fila/Main.java
+++ Codigo Base TAD Fila/Main.java
@@ -3,8 +3,11 @@ package TADFila1;
 public class Main {
 
     public static void main (String arg[]) {
-
+       
+       //Define o numero máximo de pessoas na fila
        TADFilaPessoa f = new TADFilaPessoa (100);
+       
+       //Processo para criar pessoas
        Pessoa p;
        
        p = new Pessoa (19, 'M', "Guilherme");
