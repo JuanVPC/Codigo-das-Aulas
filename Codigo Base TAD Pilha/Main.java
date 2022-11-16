7cae3b6a52f7c2a6b648af8a59c1b8e518c8bc22
JuanVPC <88857897+JuanVPC@users.noreply.github.com>
2022-11-16 16:12:57 -0300

    Update Main.java

--- Codigo Base TAD Pilha/Main.java
+++ Codigo Base TAD Pilha/Main.java
@@ -3,15 +3,21 @@ package Pilhaaa;
 public class Main
 {
 	public static void main(String[] args) {
-
+                
+		//Define o numero máximo de elementos na Pilha
 		TADPilha p = new TADPilha(8);
 		
+		
 		System.out.println("Valores Empilhados: \n");
+		
+		//Vai empilhar os números de 1 a 5
 		p.empilha(1);
 		p.empilha(2);
 		p.empilha(3);
 		p.empilha(4);
 		p.empilha(5);
+		
+		//Vai imprimir os valores empilhados
 		p.imprime();
 
 	}
