9e46197a2eb4c3cd1d51523d936dcebf3d1114a4
JuanVPC <88857897+JuanVPC@users.noreply.github.com>
2022-11-16 16:40:32 -0300

    Update No.java

--- ListaDinamica(No)/No.java
+++ ListaDinamica(No)/No.java
@@ -2,21 +2,19 @@ package No;
 
 public class No {
 
-    //Atributos
+    //Cria os atributos que receberão seus devidos valores
     int dado;
     No proximo;
     
-    //Construtor
-    
+    //Construtor dos Nós
     public No (int i) {
         dado = i;
         proximo = null;
     }
     
+    //Define o que será impresso
     public void imprime() {
-    
-    System.out.println("Valor Nó: " + dado);
-    
-    }
+        System.out.println("Valor Nó: " + dado);
+        }
        
-    }
+}
