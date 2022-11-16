5484ea3eba02a5ddac961fe1565d7179d229c9e7
JuanVPC <88857897+JuanVPC@users.noreply.github.com>
2022-11-16 17:22:38 -0300

    Update TADPilha.java

--- TADPilhaImprimeAoContrario/TADPilha.java
+++ TADPilhaImprimeAoContrario/TADPilha.java
@@ -1,30 +1,36 @@
 public class TADPilha
 {
+    //Cria os atributos que receberão seus devidos valores
     int capacidade;
     int topo;
     int dados[];
     
+    //Fala o que fazer se a Pilha estiver cheia
     boolean cheia(){
         return (topo == capacidade - 1);
     }
     
+    //Empilha os elementos
     void empilha (int d){
         cheia();
         topo++;
         dados[topo] = d;
     }
     
+    //Imprime os elementos
     void imprime(){
         for (int i=0; i<=topo; i++)
             System.out.println(dados[i]);
     }
     
+    //Imprime os elementos na ordem contrária
     void imprimeaocontrario(){
         for (int i=topo; i>=0; i--
         )
             System.out.println(dados[i]);
     }
     
+    //Define os valores da capacidade, do topo e dos dados
     TADPilha(int c){
         capacidade = c;
         topo = -1;
