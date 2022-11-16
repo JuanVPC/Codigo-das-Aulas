7a08e337fb95742529451e64f6218d65c69c7a9b
JuanVPC <88857897+JuanVPC@users.noreply.github.com>
2022-11-16 17:17:59 -0300

    Update Main.java

--- TADPilhaImprimeAoContrario/Main.java
+++ TADPilhaImprimeAoContrario/Main.java
@@ -1,17 +1,24 @@
 public class Main
 {
 	public static void main(String[] args) {
+		
+		//Define o numero máximo de elementos na Pilha
 		TADPilha p = new TADPilha(100);
 		
+		//Da um valor para "abc"
 		int abc = 100;
 		
+		//Define o que deverá ocorrer se o valor de abc for maior do que 0
 		while(abc>0){
 		    p.empilha (abc%2);
 		    abc=abc/2;
 		}
 		
+		//Imprime os elementos
 		p.imprime();
 		System.out.println();
+		
+		//Inverte a ordem de impressão dos elementos
 		p.imprimeaocontrario();
 	}
 }
